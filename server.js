df340a6ea1c101f2a01cfcc5c7e1fff0e9a81891
I am ZaKriya. <zohaib751534@gmail.com>
2025-02-17 00:29:34 +0500

    NewsLens api

---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the API!" });
+});
+
+app.get("/api/news", async (req, res) => {
+  try {
+
+    const { category } = req.query;
+    const { data } = await axios.get("https://newsapi.org/v2/top-headlines", {
+      params: {
+        country: "us",
+        category: category || "general",
+        apiKey: "b3d2f387bb514c2e80b6b706e5fb67c9",
+      },
+    });
+    res.json(data.articles);
+    // console.log(data.articles);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3001, () => console.log("Proxy server running on port 3001"));
