100ac4b29f5e827883c7a8c9c0467c5cde178218
I am ZaKriya. <zohaib751534@gmail.com>
2025-02-17 22:43:15 +0500

    cors issue

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const axios = require("axios");
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({
+  origin: "https://the-news-lens.vercel.app"
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
