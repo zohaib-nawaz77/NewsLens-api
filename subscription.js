a1ed7eb370a83ff9146860d00b471a4276c7e56c
I am ZaKriya. <zohaib751534@gmail.com>
2025-03-12 15:40:25 +0500

    reversed

--- subscription.js
+++ subscription.js
@@ -40,16 +40,12 @@ module.exports.sendWelcomeEmail = (email) => {
                         </html>`,
   };
 
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        console.error("Error sending welcome email:", err);
-        reject(err);
-      } else {
-        console.log("Welcome email sent:", info.response);
-        resolve(info);
-      }
-    });
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.error("Error sending welcome email:", err);
+    } else {
+      console.log("Welcome email sent:", info.response);
+    }
   });
 };
 
